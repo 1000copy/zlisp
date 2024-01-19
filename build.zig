1020ad010b92ae89932f3f95544e58a49bd69e36
1000copy <1000copy@gmail.com>
2024-01-19 16:05:04 +0800

    Update build.zig

--- build.zig
+++ build.zig
@@ -2,44 +2,33 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    {
+        const exe = b.addExecutable(.{
+            .name = "ziglisp",
+            .root_source_file = .{ .path = "src/lisp.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
+        b.installArtifact(exe);
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
 
-    const exe = b.addExecutable(.{
-        .name = "ziglisp",
-        .root_source_file = .{ .path = "src/lisp.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(exe);
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
     }
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    {
+        const unit_tests = b.addTest(.{
+            .root_source_file = .{ .path = "src/lisp.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
 
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/lisp.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+        const run_unit_tests = b.addRunArtifact(unit_tests);
 
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
-
-    const unit_tests1 = b.addTest(.{
-        .root_source_file = .{ .path = "test/hello.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_unit_tests1 = b.addRunArtifact(unit_tests1);
-    const test_step1 = b.step("test1", "Run unit tests");
-    test_step1.dependOn(&run_unit_tests1.step);
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_unit_tests.step);
+    }
 }
